7843baa53194b643b915be5cca72dc1ddccdc468
Gustavo Maltez <gustavomaltez37@hotmail.com>
2021-12-11 16:15:55 -0300

    chore: added aliases to webpack and tsconfig

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -2,39 +2,44 @@
 import * as path from 'path';
 import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const config: Configuration = {
-    mode: isDevelopment ? 'development' : 'production',
-    target: "web",
-    resolve: {
-        extensions: [".ts", ".js"],
+  mode: isDevelopment ? 'development' : 'production',
+  target: "web",
+  resolve: {
+    alias: {
+      "@engines": path.resolve(__dirname, '..', 'src', 'game', 'engines'),
+      "@errors": path.resolve(__dirname, '..', 'src', 'game', 'errors'),
+      "@handlers": path.resolve(__dirname, '..', 'src', 'game', 'handlers'),
+      "@logger": path.resolve(__dirname, '..', 'src', 'game', 'logger')
     },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [{ loader: "babel-loader" }],
-            },
-        ],
-    },
-    devtool: 'inline-source-map',
-    entry: path.resolve(__dirname, "..", "src", "index.ts"),
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "..", "dist"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(
-                __dirname,
-                '..',
-                'src',
-                'index.html',
-            ),
-        }),
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [{ loader: "babel-loader" }],
+      },
     ],
+  },
+  devtool: 'inline-source-map',
+  entry: path.resolve(__dirname, "..", "src", "index.ts"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "..", "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(
+        __dirname,
+        '..',
+        'src',
+        'index.html',
+      ),
+    })
+  ],
 }
 
 export default config;
