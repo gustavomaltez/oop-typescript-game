2c40df4864eb75d02d49fc03ecb6220dd2ca812f
Gustavo Maltez <gustavomaltez37@hotmail.com>
2021-12-12 00:02:22 -0300

    fix: fixed Services enum

--- src/game/logger/types.ts
+++ src/game/logger/types.ts
@@ -2,11 +2,11 @@
  * Represents all available services to log data into console.
  */
 export enum Service {
-  GAME_ENGINE = 'gameEngine',
-  CANVAS_LOADER = 'canvasLoader',
-  CANVAS_STRATEGY = 'canvasStrategy',
-  DRAWN_ENGINE_MANAGER = 'drawningEngineManager',
-  CANVAS_CONTEXT_LOADER = 'canvasContextLoader',
+  GAME_ENGINE = 'Game Engine',
+  CANVAS_LOADER = 'Canvas Loader',
+  CANVAS_STRATEGY = 'Canvas Strategy',
+  DRAWN_ENGINE_MANAGER = 'Drawing Engine Manager',
+  CANVAS_CONTEXT_LOADER = 'Canvas Context Loader',
 }
 
 export type ServiceLogColor = {
