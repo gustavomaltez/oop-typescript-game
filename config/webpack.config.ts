3acf6d8f582ec4219e2542e152dabc5d3ea13261
Gustavo Maltez <gustavomaltez37@hotmail.com>
2021-12-12 11:20:19 -0300

    chore: added copy-webpack-plugin to serve assets files

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,45 +1,50 @@
-
+/* eslint-disable import/no-extraneous-dependencies */
 import * as path from 'path';
 import { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const config: Configuration = {
   mode: isDevelopment ? 'development' : 'production',
-  target: "web",
+  target: 'web',
   resolve: {
     alias: {
-      "@engines": path.resolve(__dirname, '..', 'src', 'game', 'engines'),
-      "@errors": path.resolve(__dirname, '..', 'src', 'game', 'errors'),
-      "@handlers": path.resolve(__dirname, '..', 'src', 'game', 'handlers'),
-      "@logger": path.resolve(__dirname, '..', 'src', 'game', 'logger')
+      '@engines': path.resolve(__dirname, '..', 'src', 'game', 'engines'),
+      '@errors': path.resolve(__dirname, '..', 'src', 'game', 'errors'),
+      '@handlers': path.resolve(__dirname, '..', 'src', 'game', 'handlers'),
+      '@logger': path.resolve(__dirname, '..', 'src', 'game', 'logger'),
     },
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: [{ loader: "babel-loader" }],
+        use: [{ loader: 'babel-loader' }],
       },
     ],
   },
   devtool: 'inline-source-map',
-  entry: path.resolve(__dirname, "..", "src", "index.ts"),
+  entry: path.resolve(__dirname, '..', 'src', 'index.ts'),
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "..", "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '..', 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(
-        __dirname,
-        '..',
-        'src',
-        'index.html',
-      ),
-    })
+      template: path.resolve(__dirname, '..', 'src', 'index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '..', 'src', 'assets'),
+          to: 'assets',
+        },
+      ],
+    }),
   ],
-}
+};
 
 export default config;
