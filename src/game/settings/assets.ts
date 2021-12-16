f2ffaf0192fb5624bccfce9dfa16018f8728c4e7
Gustavo Maltez <gustavomaltez37@hotmail.com>
2021-12-16 08:34:53 -0300

    fix: fixed each asset size

--- src/game/settings/assets.ts
+++ src/game/settings/assets.ts
@@ -15,42 +15,42 @@ const playerAttackSprites: ISpriteAssetEntry[] = [
   {
     id: DefaultPlayerPosition.ATTACK_LEFT,
     spriteDimensions: {
-      height: 16,
+      height: 21,
       width: 16,
     },
     path: './assets/textures/player/attack/left.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.ATTACK_RIGHT,
     spriteDimensions: {
-      height: 16,
+      height: 21,
       width: 16,
     },
     path: './assets/textures/player/attack/right.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.ATTACK_UP,
     spriteDimensions: {
-      height: 16,
-      width: 16,
+      height: 18,
+      width: 23,
     },
     path: './assets/textures/player/attack/up.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.ATTACK_DOWN,
     spriteDimensions: {
-      height: 16,
-      width: 16,
+      height: 23,
+      width: 23,
     },
     path: './assets/textures/player/attack/down.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
 ];
 
@@ -63,7 +63,7 @@ const playerIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/player/idle/left.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.IDLE_RIGHT,
@@ -73,7 +73,7 @@ const playerIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/player/idle/right.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.IDLE_UP,
@@ -83,7 +83,7 @@ const playerIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/player/idle/up.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.IDLE_DOWN,
@@ -93,7 +93,7 @@ const playerIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/player/idle/down.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
 ];
 
@@ -101,42 +101,42 @@ const playerWalkSprites: ISpriteAssetEntry[] = [
   {
     id: DefaultPlayerPosition.WALK_LEFT,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/player/walk/left.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.WALK_RIGHT,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/player/walk/right.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.WALK_UP,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/player/walk/up.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: DefaultPlayerPosition.WALK_DOWN,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/player/walk/down.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
 ];
 
@@ -146,42 +146,42 @@ const skeletonEnemyWalkSprites: ISpriteAssetEntry[] = [
   {
     id: SkeletonEnemyPositions.WALK_LEFT,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/enemy/walk/left.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: SkeletonEnemyPositions.WALK_RIGHT,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/enemy/walk/right.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: SkeletonEnemyPositions.WALK_UP,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/enemy/walk/up.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: SkeletonEnemyPositions.WALK_DOWN,
     spriteDimensions: {
-      height: 16,
+      height: 17,
       width: 16,
     },
     path: './assets/textures/enemy/walk/down.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
 ];
 
@@ -194,7 +194,7 @@ const skeletonEnemyIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/enemy/idle/left.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: SkeletonEnemyPositions.IDLE_RIGHT,
@@ -204,7 +204,7 @@ const skeletonEnemyIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/enemy/idle/right.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: SkeletonEnemyPositions.IDLE_UP,
@@ -214,7 +214,7 @@ const skeletonEnemyIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/enemy/idle/up.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: SkeletonEnemyPositions.IDLE_DOWN,
@@ -224,7 +224,7 @@ const skeletonEnemyIdleSprites: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/enemy/idle/down.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
 ];
 
@@ -239,13 +239,13 @@ const miscAssets: ISpriteAssetEntry[] = [
     },
     path: './assets/textures/misc/chest.png',
     spriteCount: 6,
-    animationTime: 200,
+    animationTime: 80,
   },
   {
     id: Misc.BUSH,
     spriteDimensions: {
-      height: 16,
-      width: 16,
+      height: 13,
+      width: 15,
     },
     path: './assets/textures/misc/bush.png',
     spriteCount: 'SINGLE_SPRITE',
@@ -254,8 +254,8 @@ const miscAssets: ISpriteAssetEntry[] = [
   {
     id: Misc.COIN,
     spriteDimensions: {
-      height: 16,
-      width: 16,
+      height: 5,
+      width: 5,
     },
     path: './assets/textures/misc/coin.png',
     spriteCount: 'SINGLE_SPRITE',
@@ -274,8 +274,8 @@ const miscAssets: ISpriteAssetEntry[] = [
   {
     id: Misc.HEART,
     spriteDimensions: {
-      height: 16,
-      width: 16,
+      height: 7,
+      width: 7,
     },
     path: './assets/textures/misc/heart.png',
     spriteCount: 'SINGLE_SPRITE',
@@ -284,8 +284,8 @@ const miscAssets: ISpriteAssetEntry[] = [
   {
     id: Misc.ROCK,
     spriteDimensions: {
-      height: 16,
-      width: 16,
+      height: 15,
+      width: 15,
     },
     path: './assets/textures/misc/rock.png',
     spriteCount: 'SINGLE_SPRITE',
@@ -295,9 +295,19 @@ const miscAssets: ISpriteAssetEntry[] = [
     id: Misc.TREE_TRUNK,
     spriteDimensions: {
       height: 16,
-      width: 16,
+      width: 18,
     },
     path: './assets/textures/misc/tree_trunk.png',
+    spriteCount: 'SINGLE_SPRITE',
+    animationTime: 'FREEZED',
+  },
+  {
+    id: Misc.BIG_TREE,
+    spriteDimensions: {
+      height: 54,
+      width: 48,
+    },
+    path: './assets/textures/misc/big_tree.png',
     spriteCount: 'SINGLE_SPRITE',
     animationTime: 'FREEZED',
   },
